587f92e6f968592d9aa7b5718a67a456475f1a43
Kartik <kartik@hyperverge.co>
2020-10-16 14:45:22 +0530

    Add apiout and statusCodes

--- src/routes/v1/alert.js
+++ src/routes/v1/alert.js
@@ -1,8 +1,11 @@
+require('module-alias/register');
 const router = require('express').Router();
+const out = require('@lib/apiout');
+const statusCodes = require('@lib/statusCodes');
 
 // TODO : Delete this dummy API call
 router.get('/check', (req, res) => {
-  res.json({ msg: 'Aok' });
+    out.success(res, statusCodes.SUCCESS,'Aok');
 });
 
 module.exports = router;
