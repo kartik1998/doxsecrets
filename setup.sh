a2d6ac9872eef096e20a564b691ffa37a2c69237
Kartik <kartik@hyperverge.co>
2020-10-16 14:36:39 +0530

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #/bin/bash
 
-echo "We will use docker to setup gitleaks image, Ensure docker is running on your system."
-
+function setup_docker {
 while true; do
     read -p "pull docker image ? [Y/n]" yn
     case $yn in
@@ -10,6 +9,21 @@ while true; do
         * ) echo "Please answer yes or no.";;
     esac
 done 
+}
 
+function install_modules {
+while true; do
+    read -p "install npm modules ? [Y/n]" yn
+    case $yn in
+        [Yy]* ) npm install; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done 
+}
+
+install_modules
+echo "We will use docker to setup gitleaks image, Ensure docker is running on your system."
+setup_docker
 
 
