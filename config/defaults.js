e6ab4bcdb2b230e880da829bb02c1eb49e99742f
Kartik <kartik@hyperverge.co>
2020-10-17 19:12:43 +0530

    Don't show useless variables (when server starts)

--- config/defaults.js
+++ config/defaults.js
@@ -6,4 +6,5 @@ module.exports = {
     TEST_URL: 'https://github.com/jjacob27/terraform-aws-harboroneks',
     PORT: 5000,
   },
+  ignoreDisplay: ['TERM_PROGRAM', 'NODE', 'INIT_CWD', 'TERM', 'SHELL', 'TMPDIR', 'TERM_PROGRAM_VERSION', 'TERM_SESSION_ID', 'ZSH', 'SSH_AUTH_SOCK', '__CF_USER_TEXT_ENCODING', 'PAGER', 'LSCOLORS', 'PATH', 'P9K_SSH', 'P9K_TTY', 'ITERM_PROFILE', 'XPC_FLAGS', 'XPC_SERVICE_NAME', 'SHLVL', 'HOME', 'COLORFGBG', 'LC_TERMINAL_VERSION', 'ITERM_SESSION_ID', 'LESS', 'LOGNAME', 'LC_CTYPE', 'LC_TERMINAL', 'COLORTERM'],
 };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 require('module-alias/register');
 require('dotenv').config();
 require('@config/index');
-const Nconf = require('nconf')
+const {ignoreDisplay} = require('@config/defaults');
+const Nconf = require('nconf');
 
 const express = require('express');
 
@@ -13,13 +14,14 @@ const app = express();
 app.use(express.json());
 app.use('/api', routes);
 
-const displayObj = {}
-Object.entries(Nconf.get()).forEach((obj)=>{
-    if(obj[0].length>=3 && obj[0].substring(0,3)!=='npm'){
-        displayObj[obj[0]]=obj[1];
-    }
-})
+const displayObj = {};
+Object.entries(Nconf.get()).forEach((obj) => {
+  const key = obj[0], value=obj[1];
+  if (key.length >= 3 && key.substring(0, 3) !== 'npm' && !ignoreDisplay.includes(key)) {
+    displayObj[key] = value;
+  }
+});
 
-app.listen(PORT, () => { console.log(displayObj) });
+app.listen(PORT, () => { console.log(displayObj); });
 
 module.exports = app;
