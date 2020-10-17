be7b8089a9c2eb606f46c9a8698e5d2c6ae51d06
Kartik <kartik@hyperverge.co>
2020-10-17 19:12:43 +0530

    Display nconf variables when server starts
    Don't show variables starting with npm

--- config/defaults.js
+++ config/defaults.js
@@ -4,5 +4,6 @@ module.exports = {
     GITHUB_BASE_URL_USERS: 'https://api.github.com/users/',
     GIT_LEAKS_CHECK: 'docker run --rm --name=gitleaks zricethezav/gitleaks -v -r ',
     TEST_URL: 'https://github.com/jjacob27/terraform-aws-harboroneks',
+    PORT: 5000,
   },
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 require('module-alias/register');
 require('dotenv').config();
 require('@config/index');
+const Nconf = require('nconf')
 
 const express = require('express');
 
-const PORT = process.env.PORT || 5000;
+const PORT = Nconf.get().PORT || 5000;
 
 const routes = require('./routes/index');
 
@@ -12,6 +13,13 @@ const app = express();
 app.use(express.json());
 app.use('/api', routes);
 
-app.listen(PORT, () => { console.log('Server listening on port', PORT); });
+const displayObj = {}
+Object.entries(Nconf.get()).forEach((obj)=>{
+    if(obj[0].length>=3 && obj[0].substring(0,3)!=='npm'){
+        displayObj[obj[0]]=obj[1];
+    }
+})
+
+app.listen(PORT, () => { console.log(displayObj) });
 
 module.exports = app;
