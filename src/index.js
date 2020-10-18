c756cad3944e90242f0c182cec88c920a75216e2
Kartik <kartik@hyperverge.co>
2020-10-18 14:04:19 +0530

    Fix Lint

--- src/index.js
+++ src/index.js
@@ -14,6 +14,6 @@ const app = express();
 app.use(express.json());
 app.use('/api', routes);
 
-app.listen(PORT, () => { console.log(defaults) });
+app.listen(PORT, () => { console.log(defaults); });
 
 module.exports = app;
