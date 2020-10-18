c4b6698e7ea4454e2e0bac32cf4947df8d225e96
Kartik <kartik@hyperverge.co>
2020-10-18 18:20:19 +0530

    Remove try-catch block from AlertService

--- src/alert/service.js
+++ src/alert/service.js
@@ -13,31 +13,26 @@ class AlertService {
     let leakCode = 0;
     const output = [];
     const response = await axios.get(`${GITHUB_BASE_URL_USERS + userName}/repos`);
-
-    try {
-      response.data.forEach((res) => {
-        const { stdout, stderr, code } = shell.exec(`${GIT_LEAKS_CHECK}${res.html_url}`, { silent: true });
-        if (code === 1) {
-          leakCode = 1;
-          output.push({
-            info: fetchJson(stdout),
-            url: res.html_url,
-            collaborators_url: res.collaborators_url,
-            teams_url: res.teams_url,
-            created_at: res.created_at,
-            updated_at: res.updated_at,
-            pushed_at: res.pushed_at,
-          });
-        } else if (code === 2 && leakCode !== 1) {
-          leakCode = 2;
-        }
-        console.log(stderr);
-      });
-      responseObj.output = output;
-      responseObj.leakCode = leakCode;
-    } catch (err) {
-      console.error(err);
-    }
+    response.data.forEach((res) => {
+      const { stdout, stderr, code } = shell.exec(`${GIT_LEAKS_CHECK}${res.html_url}`, { silent: true });
+      if (code === 1) {
+        leakCode = 1;
+        output.push({
+          info: fetchJson(stdout),
+          url: res.html_url,
+          collaborators_url: res.collaborators_url,
+          teams_url: res.teams_url,
+          created_at: res.created_at,
+          updated_at: res.updated_at,
+          pushed_at: res.pushed_at,
+        });
+      } else if (code === 2 && leakCode !== 1) {
+        leakCode = 2;
+      }
+      console.log(stderr);
+    });
+    responseObj.output = output;
+    responseObj.leakCode = leakCode;
     return responseObj;
   }
 
