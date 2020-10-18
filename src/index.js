d0889c1a25f7328ad335d768b947b7e34a31d52d
Kartik <kartik@hyperverge.co>
2020-10-18 14:03:12 +0530

    Revert "e6ab4.." and only print defaults when server starts
    This reverts commit e6ab4bcdb2b230e880da829bb02c1eb49e99742f.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 require('module-alias/register');
 require('dotenv').config();
 require('@config/index');
-const { ignoreDisplay } = require('@config/defaults');
 const Nconf = require('nconf');
+const { default: defaults } = require('@config/defaults');
 
 const express = require('express');
 
@@ -14,15 +14,6 @@ const app = express();
 app.use(express.json());
 app.use('/api', routes);
 
-const displayObj = {};
-Object.entries(Nconf.get()).forEach((obj) => {
-  const key = obj[0]; const
-    value = obj[1];
-  if (key.length >= 3 && key.substring(0, 3) !== 'npm' && !ignoreDisplay.includes(key)) {
-    displayObj[key] = value;
-  }
-});
-
-app.listen(PORT, () => { console.log(displayObj); });
+app.listen(PORT, () => { console.log(defaults) });
 
 module.exports = app;
